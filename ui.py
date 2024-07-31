d191a5a588e9736bf86cb0b055339c5f4926f8b5
chunsj75 <chunsj75@naver.com>
2024-07-31 22:46:34 +0900

    Modify btn1, le1 and method name to setDisplay in ui.py

--- ui.py
+++ ui.py
@@ -1,4 +1,4 @@
-# ch 6.2.1 ui.py
+# ch 6.3.1 ui.py
 from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit, QHBoxLayout, QLineEdit, QComboBox) # QLineEdit, QComboBox 추가
 from PyQt5.QtGui import QIcon
 from PyQt5 import QtCore # 모듈 추가
@@ -15,6 +15,8 @@ class View(QWidget):
         
         self.le1 = QLineEdit('0', self) # 라인 에디트1 추가
         self.le1.setAlignment(QtCore.Qt.AlignRight) # 라인 에디트1 문자열 배치 설정
+        self.le1.setFocus(True) # 포커스 설정
+        self.le1.selectAll() # 텍스트 전체 선택
 
         self.le2 = QLineEdit('0', self) # 라인 에디트2 추가
         self.le2.setAlignment(QtCore.Qt.AlignRight) # 라인 에디트2 문자열 배치 설정
@@ -27,7 +29,7 @@ class View(QWidget):
         hbox_formular.addWidget(self.cb)
         hbox_formular.addWidget(self.le2)
 
-        self.btn1 = QPushButton('Message', self)
+        self.btn1 = QPushButton('Calc', self) # 버튼 이름 변경
         self.btn2 = QPushButton('Clear', self)
         
         hbox = QHBoxLayout()
@@ -48,7 +50,7 @@ class View(QWidget):
         self.resize(256, 256)
         self.show()
 
-    def activateMessage(self):
+    def setDisplay(self): # 메서드 이름 변경
         self.te1.appendPlainText("Button clicked!")
 
     def clearMessage(self):
